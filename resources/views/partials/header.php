9cd4397044b23e04c689f2d18269fddec3fb40fd
nikondi <nikondi@narod.ru>
2026-01-29 01:43:11 +0300

    Header start

---
+++ resources/views/partials/header.php
@@ -0,0 +1,30 @@
+<header class="header">
+    <div class="container">
+        <div class="header-inner">
+            <a href="/" class="header-logo">
+                <img src="/assets/img/logo.svg" alt=""/>
+            </a>
+            <nav class="header-nav">
+                <ul>
+                    <li><a href="#">Каталог</a></li>
+                    <li><a href="#">Заявки</a></li>
+                    <li><a href="#">Аналитика</a></li>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">Фото и видео банк</a></li>
+                </ul>
+            </nav>
+            <div class="header-actions">
+                <!-- TODO: lang switcher -->
+                <div class="header-action lang-switcher">
+                    <span>RU</span>
+                    <span><img src="/assets/img/icons/chevron-down.svg" alt=""></span>
+                </div>
+
+                <a href="#" class="header-action">
+                    <img src="/assets/img/icons/user.svg">
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+<!--<div class="header-placeholder"></div>-->

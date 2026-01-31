04b604dbc845f49df2eed3dce591dc51f716ae1e
nikondi <nikondi@narod.ru>
2026-01-31 03:01:03 +0300

    Cleanup

--- app/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Facades\Vite;
-
-function vite(string|array $asset): string
-{
-    return Vite::getInstance()->tags($asset);
-}
--- routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Controllers\WelcomeController;
-use App\Router;
-
-Router::get('/', WelcomeController::class);
